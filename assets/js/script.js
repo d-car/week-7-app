453b445a2dec5aa60361898bed22d46b64c6c2fe
d-car <danielscarstensen@gmail.com>
2018-07-12 00:31:40 -0500

    added html and linked firebase

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDcg7G7FCEwK1DLhuQYYC0Ge9-N5dqfGmY",
+    authDomain: "week-7-train-b61c9.firebaseapp.com",
+    databaseURL: "https://week-7-train-b61c9.firebaseio.com",
+    projectId: "week-7-train-b61c9",
+    storageBucket: "week-7-train-b61c9.appspot.com",
+    messagingSenderId: "569981737817"
+  };
+  firebase.initializeApp(config);
