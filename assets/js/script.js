a95330149d4e1f6f375018955bfcf05ed49e4892
d-car <danielscarstensen@gmail.com>
2018-07-14 23:02:27 -0500

    added styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,46 +11,19 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
+//clears input fields
 function clearInputs() {
   $("#name-input").val("");
   $("#destination-input").val("");
   $("#start-input").val("");
   $("#frequency-input").val("");
 }
-
+//runs clock
 function displayTime() {
   var time = moment().format('HH:mm:ss');
   $('#currentTime').html(time);
   setTimeout(displayTime, 1000);
 }
-
-// database.ref().on("child_added", function(snapshot) {
-  
-//   // Get reference to existing tbody element, create a new table row element
-//   var tBody = $("tbody");
-//   var tRow = $("<tr>");
-  
-
-//   // Methods run on jQuery selectors return the selector they we run on
-//   // This is why we can create and save a reference to a td in the same statement we update its text
-//   var trainNameTd = $("<td>").text(snapshot.val().trainName);
-//   var trainDestinationTd = $("<td>").text(snapshot.val().trainDestination);
-//   var trainFrequencyTd = $("<td>").text(snapshot.val().trainFrequency);
-//   var trainBeginTd = $("<td>").text(snapshot.val().trainBegin);
-
-
-  
- 
-
-
-//   // Append the newly created table data to the table row
-//   tRow.append(trainNameTd, trainDestinationTd, trainFrequencyTd,);
-//   //  nextTrainTd, minutesAwayTd);
-
-//   // Append the table row to the table body
-//   tBody.append(tRow);
-
-// });
 
 // Capture Button Click
 $("#submit").on("click", function(event) {
@@ -63,26 +36,6 @@ $("#submit").on("click", function(event) {
   var trainFrequency = $("#frequency-input").val().trim();
   
   clearInputs();
-
-  //Add clock to page
-  // var fixTime = moment(time, "hh:mm").subtract(1, "years");
-  // var currentMinute = moment();
-  // //append to html
-  // $("#currentTime").html(" Current Time : " + currentMinute.format("hh:mm"));
-
-  // var timeDifference = moment().diff(moment(fixTime), "minutes");
-  // var timeRemaining = timeDifference % trainFrequency
-  // var minutesAway = trainFrequency - timeRemaining;
-  // var nextTrain = moment().add(minutesAway, "minutes")
-  // var arrivalTime = moment(nextTrain).format("HH:mm");
-  
-  // console.log(trainName);
-  // console.log(trainDestination)
-  // console.log(trainBegin)
-  // console.log(trainFrequency)
-  // console.log(nextTrain);
-  // console.log(arrivalTime);
-  // console.log(minutesAway);
 
   // Code for "Setting values in the database"
   database.ref().push({
@@ -97,64 +50,48 @@ database.ref().on("child_added", function(childSnapshot) {
   console.log(childSnapshot.val());
 
 
-  //create new variables for clean build from childSnapshot of data from firebase
+  //create new variables from snapshot of firebase data
 
   var name = childSnapshot.val().trainName;
   var destination = childSnapshot.val().trainDestination;
   var frequency = childSnapshot.val().trainFrequency;
-  var time = childSnapshot.val().arrivalTime;
+  var time = childSnapshot.val().trainBegin;
 
-
-  //code in math to find the next train time and minutes until next arrival based off of frequency value and first train time value.
-
-  // convert first train time back a year to make sure it is set before current time before pushing to firebase.
-
+  //convert time before pushing to database.
   var fixTime = moment(time, "HH:mm").subtract(1, "years");
-  console.log(fixTime);
-
-  //set a variable equal to the current time from moment.js
+  // console.log(fixTime);
 
   var currentTime = moment();
-  console.log("Current Time: " + moment(currentTime).format("HH:mm"));
+  // console.log("Current Time: " + moment(currentTime).format("HH:mm"));
 
-  //post current time to div for reference
+  //current time
+  $("#currentTime").html(moment(currentTime).format("HH:mm"));
 
-  $("#currentTime").html("Current Time: " + moment(currentTime).format("HH:mm"));
-
-  //find the difference between the first train time and the current time
-
+  //finds diff between og train and current time
   var timeDiff = moment() - frequency;
   console.log("Difference In Time: " + timeDiff);
 
-  //find the time apart by finding the remainder of the time difference and the frequency - use modal to get whole remainder number
-
+  
   var timeRemainder = timeDiff % frequency;
   console.log(timeRemainder);
 
-  //find the minutes until the next train
-
+  //time until next train for table
   var nextTrainMin = frequency - timeRemainder;
   console.log("Minutes Till Train: " + nextTrainMin);
 
-  //find the time of the next train arrival
-
+  //time of next train in HH:mm format for table
   var nextTrainAdd = moment().add(nextTrainMin, "minutes");
   var nextTrainArr = moment(nextTrainAdd).format("HH:mm");
   console.log("Arrival Time: " + nextTrainArr);
 
-  //prepend all information for train data submitted by user
-
+  //appends all information for train data submitted by user
   $("#trainSchedule").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrainArr + "</td><td>" + nextTrainMin + "</td></tr>");
-
-
-}, function(err) {
-  console.log(err);
 });
 
 
 
 
-
+//display clock on document load
 $(document).ready(function() {
   displayTime();
 });
